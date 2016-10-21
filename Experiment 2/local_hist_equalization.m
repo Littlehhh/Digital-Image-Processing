95d524600704b2e72485e6b31324fb7346ba4fdf
Littlehhh <wanghuichn@yahoo.com>
2016-10-21 11:34:18 +0800

    ADD local hist eq

---
+++ Experiment 2/local_hist_equalization.m
@@ -0,0 +1,27 @@
+function Output = local_hist_equalization(Input)
+
+%局部直方图均衡
+% clc
+% close all
+% clear all
+% Input = InputPicture();
+% figure
+% imhist(Input);
+
+[length,width] = size(Input);
+Output = zeros(length,width);
+
+
+%创建3*3模板
+B = zeros(3,3);
+
+for i = 2:length-1;
+    for j = 2:width-1;
+       
+       B = myHisteq(Input(i-1:i+1,j-1:j+1)); 
+       Output(i,j) =  B(2,2);
+        
+    end
+end
+Output = uint8(Output);
+
--- Experiment 2/myHisteq.m
+++ Experiment 2/myHisteq.m
@@ -1,58 +1,65 @@
-
-clc
-close all
-clear all
-Input = InputPicture();
-figure
-imhist(Input);
-
+function Output = myHisteq(Input,fun,Y)
+%fun = 1  直方图均衡
+%fun = 2  直方图匹配
+%默认调用为直方图均衡
+%Y为输入的匹配直方图
+if nargin < 2 
+   fun = 1; 
+end
+if nargin > 2 
+   fun = 2;
+end
+% Input = InputPicture();
+% figure
+% imhist(Input);
 [lenth,width] = size(Input);
 Output = zeros(lenth,width);
 %计算各灰度级概率密度
 CDF = caculateCDF(Input);
+if fun == 2
+    %直方图匹配
 
-%直方图匹配
+    %双高斯函数生成
+%     Y = zeros(1,256);
+%     Y1 = myGaussFun(0:127,15,50);
+%     Y2 = myGaussFun(0:127,80,15);
+%     Y(:,1:128) = Y1;
+%     Y(:,129:256) = 0.1*Y2;
+    %Y = myGaussFun(0:255,180,100);
+    Y = Y/sum(Y); %归一化
+%     figure
+%     plot(1:256,Y)
+    CDFRef = zeros(1,256);
+    CDFRef(1)=Y(1);
+    for i=2:256;
+        CDFRef(i)= Y(i)+CDFRef(i-1);
+    end
 
-%双高斯函数生成
-Y = zeros(1,256);
-Y1 = myGaussFun(0:127,15,50);
-Y2 = myGaussFun(0:127,80,15);
-Y(:,1:128) = Y1;
-Y(:,129:256) = 0.1*Y2;
-Y = Y/sum(Y); %归一化
-figure
-plot(1:256,Y)
-CDFRef = zeros(1,256);
-CDFRef(1)=Y(1);
-for i=2:256;
-    CDFRef(i)= Y(i)+CDFRef(i-1);
-end
-
-X = zeros(1,256);
-for i=1:256
-   [x,n] = min(abs(CDF(i) - CDFRef));
-   X(i) = n-1;
-end
-for i=1:lenth
-    for j=1:width
-        Output(i,j) = X(Input(i,j)+1);
+    X = zeros(1,256);
+    for i=1:256
+       [x,n] = min(abs(CDF(i) - CDFRef));
+       X(i) = n-1;
+    end
+    for i=1:lenth
+        for j=1:width
+            Output(i,j) = X(Input(i,j)+1);
+        end
     end
 end
-
-
-% %直方图均衡
-% Eq = round(CDF*255);
-% for i=1:lenth
-%     for j=1:width
-%         Output(i,j) = Eq(Input(i,j)+1);
-%     end
-% end
-
+if fun == 1
+    %直方图均衡
+    Eq = round(CDF*255);
+    for i=1:lenth
+        for j=1:width
+            Output(i,j) = Eq(Input(i,j)+1);
+        end
+    end
+end
 Output = uint8(Output);
-figure
-imhist(Output)
-figure
-imshow(Output)
+% figure
+% imhist(Output)
+% figure
+% imshow(Output)
 
 % Iout=histeq(Input,Y); % matlab自带直方图匹配
 % 
