8de19b84f18d6a6b6528927627c31aebf0f1c1e1
Littlehhh <wanghuichn@yahoo.com>
2016-11-17 21:50:13 +0800

    Update

--- Experiment 4/myFresnel_diffraction.m
+++ Experiment 4/myFresnel_diffraction.m
@@ -15,10 +15,12 @@ yy = -width/2:dx:width/2-dy;
 
 [x,y] = meshgrid(xx,yy);
 h = exp(1i*k*(x.^2+y.^2)./(2*z));
-
+% tic
 Output = fftshift(ifft2(fft2(Input).*fft2(h)));  %利用卷积定理将菲涅尔衍射转化为傅里叶变换的乘积
-% Output = conv2(Input,h,'same'); 
-
+% toc
+% tic
+%  Output2 = conv2(Input,h,'same'); 
+% toc
 Out = exp(1i*k*z).*Output/(1i*lamda*z);
 
 % figure
--- Experiment 4/testdiffraction.m
+++ Experiment 4/testdiffraction.m
@@ -4,20 +4,20 @@
 Input = myGaussian2D(60,60);
 % Input = myRect2D(600,600,10000,2);
 % Out = myFraunhofer_diffraction(Input,532,10000);
-
+% Out = myFresnel_diffraction(Input,532,880);
 close all
 pause('on');
 figure
-for i=880:980;
+for i=880:1000;
     Out = myFresnel_diffraction(Input,532,i);
     title(['z=',num2str(i),'mm']);
     pause(0.01);
 end
-close all
-pause('on');
-figure
-for i=5000:200:50000;
-    Out = myFraunhofer_diffraction(Input,532,i);
-    title(['z=',num2str(i),'mm']);
-    pause(0.01);
-end
+% close all
+% pause('on');
+% figure
+% for i=5000:200:50000;
+%     Out = myFraunhofer_diffraction(Input,532,i);
+%     title(['z=',num2str(i),'mm']);
+%     pause(0.01);
+% end
